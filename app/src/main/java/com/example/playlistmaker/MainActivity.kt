8d26e8939480d49cc249d0185cb63099d2506e63
Megrief <eu.pim@mail.ru>
2023-04-05 07:36:06 +0300

    Сверстан главный экран и экран настроек. Добавлены "тосты"

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settings = findViewById<Button>(R.id.settings)
+        val media = findViewById<Button>(R.id.media)
+        val search = findViewById<Button>(R.id.search)
+
+        search.setOnClickListener {
+            Toast.makeText(this, "Looking for something...", Toast.LENGTH_LONG).show()
+        }
+
+        val buttonOnClickListener = object : OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Open media", Toast.LENGTH_SHORT).show()
+            }
+        }
+        media.setOnClickListener(buttonOnClickListener)
+
+        settings.setOnClickListener {
+            Toast.makeText(this, "Open settings", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
