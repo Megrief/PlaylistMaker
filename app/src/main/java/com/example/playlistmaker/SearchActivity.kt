f2ea83bfc31c2c1845e19f0c0d8c189b2c4204f8
Megrief <eu.pim@mail.ru>
2023-04-25 11:55:15 +0300

    Реализована строка поиска, завершена верстка экрана поиска.

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -2,10 +2,55 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
+    private var savedValue: String = ""
+    companion object {
+        const val SEARCH_BAR_STATE = "SEARCH_BAR_STATE"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchBar = findViewById<EditText>(R.id.search_bar)
+        val clearButton = findViewById<ImageButton>(R.id.clear_button)
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                if (!s.isNullOrEmpty()) savedValue = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+            }
+        }
+
+        if (savedInstanceState != null) {
+            searchBar.setText(savedInstanceState.getString(SEARCH_BAR_STATE))
+        }
+
+        searchBar.addTextChangedListener(textWatcher)
+
+        clearButton.setOnClickListener {
+            searchBar.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_BAR_STATE, savedValue)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
     }
 }
