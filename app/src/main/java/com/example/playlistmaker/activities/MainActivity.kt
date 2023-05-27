518a5254514944836e536d2093e99a0f652915d4
Megrief <eu.pim@mail.ru>
2023-05-27 08:02:22 +0300

    Корректировки в оформлении кода: - переменные View вынесены из метода onCreate в поля класса; - обработка нажатий вынесена в отдельные методы.

--- app/src/main/java/com/example/playlistmaker/activities/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activities/MainActivity.kt
@@ -7,13 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
 
 class MainActivity : AppCompatActivity() {
+    private val settingsButton by lazy { findViewById<Button>(R.id.settings) }
+    private val mediaButton by lazy { findViewById<Button>(R.id.media) }
+    private val searchButton by lazy { findViewById<Button>(R.id.search) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val settingsButton = findViewById<Button>(R.id.settings)
-        val mediaButton = findViewById<Button>(R.id.media)
-        val searchButton = findViewById<Button>(R.id.search)
 
         searchButton.setOnClickListener {
             startActivity(Intent(this, SearchActivity::class.java))
--- app/src/main/java/com/example/playlistmaker/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activities/SettingsActivity.kt
@@ -5,33 +5,29 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 import com.example.playlistmaker.R
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
+    private val toolbar by lazy { findViewById<MaterialToolbar>(R.id.toolbar) }
+    private val themeSwitcher by lazy { findViewById<SwitchMaterial>(R.id.dark_theme) }
+    private val shareButton by lazy { findViewById<TextView>(R.id.share) }
+    private val supportButton by lazy { findViewById<TextView>(R.id.support) }
+    private val userAgreementButton by lazy { findViewById<TextView>(R.id.user_agreement) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        toolbar.setNavigationOnClickListener { finish() }
+        configureShareButton()
+        configureUserAgreementButton()
+        configureSupportButton()
+        configureThemeSwitcher()
+    }
 
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        val darkThemeSwitch = findViewById<SwitchCompat>(R.id.dark_theme)
-        val shareButton = findViewById<TextView>(R.id.share)
-        val supportButton = findViewById<TextView>(R.id.support)
-        val userAgreementButton = findViewById<TextView>(R.id.user_agreement)
-
-        toolbar.setNavigationOnClickListener {
-            finish()
-        }
-
-        shareButton.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_SEND).apply {
-                this.type = "text/plain"
-                this.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_developer))
-            })
-        }
-
+    private fun configureSupportButton() {
         supportButton.setOnClickListener {
             startActivity(Intent(Intent.ACTION_SENDTO).apply {
                 this.data = Uri.parse("mailto:")
@@ -40,11 +36,24 @@ class SettingsActivity : AppCompatActivity() {
                 this.putExtra(Intent.EXTRA_TEXT, getString(R.string.supp_message_content))
             })
         }
-
+    }
+    private fun configureShareButton() {
+        shareButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_SEND).apply {
+                this.type = "text/plain"
+                this.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_developer))
+            })
+        }
+    }
+    private fun configureUserAgreementButton() {
         userAgreementButton.setOnClickListener {
             val url = Uri.parse(getString(R.string.practicum_offer))
             startActivity(Intent(Intent.ACTION_VIEW, url))
         }
-
+    }
+    private fun configureThemeSwitcher() {
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            TODO()
+        }
     }
 }
