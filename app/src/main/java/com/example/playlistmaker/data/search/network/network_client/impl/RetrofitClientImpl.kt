3eefd1d45c4f44be12e9f2e6a31b54007859018c
Megrief <eu.pim@mail.ru>
2023-09-18 13:27:33 +0300

    Переработка приложения на SingleActivity: - NavigationGraph - BottomNavigationMenu - Заменена верстка MainActivity

--- app/src/main/java/com/example/playlistmaker/data/search/network/network_client/impl/RetrofitClientImpl.kt
+++ app/src/main/java/com/example/playlistmaker/data/search/network/network_client/impl/RetrofitClientImpl.kt
@@ -4,17 +4,9 @@ import com.example.playlistmaker.data.search.dto.TrackSearchRequest
 import com.example.playlistmaker.data.search.network.Response
 import com.example.playlistmaker.data.search.network.api.ITunesApiService
 import com.example.playlistmaker.data.search.network.network_client.NetworkClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
-class RetrofitClientImpl : NetworkClient {
-    private val baseUrl = "https://itunes.apple.com"
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val apiService = retrofit.create(ITunesApiService::class.java)
+class RetrofitClientImpl(private val apiService: ITunesApiService) : NetworkClient {
 
     override fun doSearch(dto: TrackSearchRequest): Response {
         val response = try {
